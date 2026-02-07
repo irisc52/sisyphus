f574f86b37a3a87bd3068299e2f28cff6a294d22
irisc52 <iristtchen@gmail.com>
2026-02-06 22:21:40 -0500

    dashboarddddd and popup

--- uiux/vite.config.js
+++ uiux/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'popup.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+      },
+    },
+  },
 })
